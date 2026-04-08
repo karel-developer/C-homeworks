ed928586dcbf28215447a1dfe0e28c5ec1a13bc5
Karel Bejarano <karel_dev@outlook.com>
2026-04-08 16:17:41 -0400

    Update Producto.hpp

--- Primer Parcial/Dominio/Producto.hpp
+++ Primer Parcial/Dominio/Producto.hpp
@@ -44,27 +44,29 @@ class Producto {
             stock += change;
         }
         void mostrarProducto() {
+            cout << "ID: " << getId() << '\n';
             cout << "Nombre: " << getNombre() << '\n';
             cout << "Categoria: " << getCategoria() << '\n';
             cout << "Precio: " << getPrecio() << '\n';
+            cout << "Stock: " << getStock() << '\n';
             cout << "------------------------\n";
         }
     };
 
-inline void to_json(nlohmann::json& j, const Producto& p) {
-    j = nlohmann::json{
-        {"id", p.getId()},
-        {"nombre", p.getNombre()},
-        {"categoria", p.getCategoria()},
-        {"precio", p.getPrecio()},
-        {"stock", p.getStock()}
-    };
-}
+    inline void to_json(nlohmann::json& j, const Producto& p) {
+        j = nlohmann::json{
+            {"id", p.getId()},
+            {"categoria", p.getCategoria()},
+            {"nombre", p.getNombre()},
+            {"precio", p.getPrecio()},
+            {"stock", p.getStock()}
+        };
+    }
 
-inline void from_json(const nlohmann::json& j, Producto& p) {
-    p.setId(j.at("id").get<int>());
-    p.setNombre(j.at("nombre").get<string>());
-    p.setCategoria(j.at("categoria").get<string>());
-    p.setPrecio(j.at("precio").get<double>());
-    p.setStock(j.at("stock").get<int>());
-}
+    inline void from_json(const nlohmann::json& j, Producto& p) {
+        p.setId(j.at("id").get<int>());
+        p.setCategoria(j.at("categoria").get<string>());
+        p.setNombre(j.at("nombre").get<string>());
+        p.setPrecio(j.at("precio").get<double>());
+        p.setStock(j.at("stock").get<int>());
+    }
